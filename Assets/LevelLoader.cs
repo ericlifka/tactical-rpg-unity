47cd896a81bdd63aa79f018ab2ae56f361cab6ff
Eric Lifka <eric.lifka@inin.com>
2017-04-01 00:10:22 -0400

    refactor to use a Main class as the hub for things

--- Assets/LevelLoader.cs
+++ Assets/LevelLoader.cs
@@ -3,94 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour {
-	public GameObject tilePrefab;
-	public Camera camera;
 
-	private TrackCameraToActiveTile cameraTracker;
+	// Use this for initialization
+	void Start () {
 
-	private float tileScale = 0.1F;
-	private float tileSpacing = 2.1F;
-
-	private GameObject[,] tiles;
-	private int gridWidth = 9;
-	private int gridHeight = 5;
-	private int activeX = 0;
-	private int activeY = 0;
-
-	private bool inputReleased = true;
-
-	void Start() {
-		cameraTracker = camera.GetComponent<TrackCameraToActiveTile>();
-
-		tiles = new GameObject[gridWidth, gridHeight];
-
-		for (int x = 0; x < gridWidth; x++) {
-			for (int y = 0; y < gridHeight; y++) {
-
-				GameObject tile = Instantiate(tilePrefab);
-				tile.transform.position = new Vector2(tileSpacing * x, tileSpacing * y);
-				tile.transform.localScale = new Vector2(0.1F, 0.1F);
-
-				tiles[x, y] = tile;
-			}
-		}
-
-		activeX = gridWidth / 2;
-		activeY = gridHeight / 2;
-		switchActiveTile(tiles[activeX, activeY], null);
 	}
-	
+
 	// Update is called once per frame
-	void Update() {
-		float valueX = Input.GetAxis("Horizontal");
-		float valueY = Input.GetAxis("Vertical");
+	void Update () {
 
-		GameObject currentActive = tiles[activeX, activeY];
-		GameObject newActive = null;
-
-		if (!inputReleased && valueX == 0 && valueY == 0) {
-			inputReleased = true;
-
-		}
-		else if (inputReleased && valueX < 0 && activeX > 0) {
-			inputReleased = false;
-			activeX--;
-			newActive = tiles[activeX, activeY];
-
-		}
-		else if (inputReleased && valueY < 0 && activeY > 0) {
-			inputReleased = false;
-			activeY--;
-			newActive = tiles[activeX, activeY];
-
-		}
-		else if (inputReleased && valueX > 0 && activeX < gridWidth - 1) {
-			inputReleased = false;
-			activeX++;
-			newActive = tiles[activeX, activeY];
-
-		}
-		else if (inputReleased && valueY > 0 && activeY < gridHeight - 1) {
-			inputReleased = false;
-			activeY++;
-			newActive = tiles[activeX, activeY];
-		}
-
-		if (newActive && newActive != currentActive) {
-			switchActiveTile(newActive, currentActive);
-		}
-	}
-
-	private void switchActiveTile(GameObject newActive, GameObject oldActive) {
-		if (oldActive) {
-			oldActive.GetComponent<TileActiveStateManager>().switchToInactive();
-		}
-
-		if (newActive) {
-			newActive.GetComponent<TileActiveStateManager>().switchToActive();
-
-			cameraTracker.trackToPosition(newActive.transform.position);
-		}
 	}
 }
 	
